8aadc6a8144c737f35811ab652172b6a07c5eb44
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-07-06 16:46:14 -0400

    Time: 15 ms (95.09%), Space: 11.6 MB (93.88%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if (intervals.size() < 2)
+            return true;
+        
+        sort(intervals.begin(), intervals.end());
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] < intervals[i - 1][1])
+                return false;
+        }
+        
+        return true;
+    }
+};
