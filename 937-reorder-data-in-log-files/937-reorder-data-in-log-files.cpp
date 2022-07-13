73b077a77f0701e0c431e97d8bd3039d45799cef
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-07-13 12:33:30 -0400

    Time: 20 ms (46.73%), Space: 10 MB (98.15%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        std::stable_sort(logs.begin(), logs.end(), [](auto& s1, auto& s2) {
+            int pos1 = s1.find_first_of(' ');
+            int pos2 = s2.find_first_of(' ');
+            if (isdigit(s1[pos1 + 1]) && isdigit(s2[pos2 + 1])) {
+                cout << "iiii";
+                return false;
+            } else if (!isdigit(s1[pos1 + 1]) && !isdigit(s2[pos2 + 1])) {
+                string_view sv1(s1.c_str() + pos1);
+                string_view sv2(s2.c_str() + pos2);
+                if (sv1 == sv2) {
+                    string_view sv01(s1.c_str(), pos1);
+                    string_view sv02(s2.c_str(), pos2);
+                    return sv01 < sv02;
+                }
+                
+                return sv1 < sv2;
+            } else if (isdigit(s1[pos1 + 1]) && !isdigit(s2[pos2 + 1])) {
+                cout << "444";
+                return false;
+            } else if (!isdigit(s1[pos1 + 1]) && isdigit(s2[pos2 + 1])) {
+                cout << "999";
+                return true;
+            } 
+            
+            return false;
+        });
+        
+        return logs;
+    }
+    
+    static bool mycmp(string &s1, string &s2) {
+        int pos1 = s1.find_first_of(' ');
+        int pos2 = s2.find_first_of(' ');
+        if (isdigit(s1[pos1 + 1]) && isdigit(s2[pos2 + 1])) {
+            cout << "iiii";
+            return true;
+        } else if (!isdigit(s1[pos1 + 1]) && !isdigit(s2[pos2 + 1])) {
+            string_view sv1(s1.c_str() + pos1);
+            string_view sv2(s2.c_str() + pos2);
+            if (sv1 == sv2) {
+                string_view sv01(s1.c_str(), pos1);
+                string_view sv02(s2.c_str(), pos2);
+                return sv01 < sv02;
+            }
+            
+            return sv1 < sv2;
+        } else {
+            if (isdigit(s1[pos2 + 1])) {
+                return false;
+            } else { // isdigit(s2[pos1 + 1])
+                return true;
+            }
+        }
+    }
+        
+};
