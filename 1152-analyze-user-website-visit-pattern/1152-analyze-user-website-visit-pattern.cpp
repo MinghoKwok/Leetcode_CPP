0fc909fb7b0d0c71358d8119306c85cd4355aa9f
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-07-21 22:19:01 -0400

    Time: 62 ms (49.51%), Space: 18.3 MB (69.51%) - LeetHub

---
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<string> mostVisitedPattern(vector<string>& username, vector<int>& timestamp, vector<string>& website) {
+        vector<string> res;
+        unordered_map<string, map<int, string>> mp; // username -> (time -> website)
+        unordered_map<string, int> cnt; // pattern -> count
+        
+        for (int i = 0; i < username.size(); i++) {
+            mp[username[i]][timestamp[i]] = website[i];
+        }
+        
+        for (auto it_mp = mp.begin(); it_mp != mp.end(); it_mp++) {
+            set<string> cur_user;
+            for (auto it1 = it_mp->second.begin(); it1 != it_mp->second.end(); it1++) {
+                for (auto it2 = next(it1); it2 != it_mp->second.end(); it2++) {
+                    for (auto it3 = next(it2); it3 != it_mp->second.end(); it3++) {
+                        string str = it1->second + '$' + it2->second + '@' + it3->second;
+                        cur_user.insert(str);
+                    }
+                }
+            }
+            
+            for (auto str : cur_user) {
+                cnt[str]++;
+            }
+        }
+        
+        
+        string res_str;
+        int max_cnt = -1;
+        for (auto it = cnt.begin(); it != cnt.end(); it++) {
+            if (max_cnt < it->second) {
+                max_cnt = it->second;
+                res_str = it->first;
+            } else if (max_cnt == it->second) {
+                if (res_str > it->first)
+                    res_str = it->first;
+            } else {
+                continue;
+            }
+        }
+        int pos1 = res_str.find('$');
+        int pos2 = res_str.find('@');
+        res.push_back(res_str.substr(0, pos1));
+        res.push_back(res_str.substr(pos1 + 1, pos2 - pos1 - 1));
+        res.push_back(res_str.substr(pos2 + 1));
+        
+        
+        return res;
+    }
+    
+    
+};
