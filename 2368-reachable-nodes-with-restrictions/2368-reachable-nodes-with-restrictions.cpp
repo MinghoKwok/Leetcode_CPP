1170e2676341e62a1cbe37cf77fdc22cbee39ec9
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-08-07 21:52:11 -0400

    Time: 1273 ms (11.11%), Space: 192.2 MB (11.11%) - LeetHub

---
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    unordered_set<int> set_res;
+    unordered_set<int> visited;
+    vector<vector<int>> vec_edges;
+public:
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        sort(edges.begin(), edges.end());
+        vec_edges.resize(n);
+        
+        // construct graph
+        for (int i = 0; i < edges.size(); i++) {
+            vec_edges[edges[i][0]].push_back(edges[i][1]);
+            vec_edges[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        // Transfer restricted vector to set
+        // for (int i = 0; i < restricted.size(); i++) {
+        //     set_res.insert(restricted[i]);
+        // }
+        set_res = unordered_set<int>(restricted.begin(), restricted.end());
+        
+        // dfs
+        dfs(0);
+        
+        
+        
+        return visited.size();
+    }
+    
+    void dfs(int num) {
+        visited.insert(num);
+        for (int i = 0; i < vec_edges[num].size(); i++) {
+            int next = vec_edges[num][i];
+            if (visited.find(next) == visited.end() && set_res.find(next) == set_res.end()) {
+                dfs(next);
+            }
+        }
+    }
+};
+
