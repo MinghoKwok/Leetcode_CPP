b9ef72a81a2dc51d7ac86363f7dedac672711b79
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-08-07 22:04:16 -0400

    Time: 609 ms (55.56%), Space: 152 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,10 +1,12 @@
 class Solution {
     //unordered_set<int> set_res;
-    unordered_set<int> visited;
+    vector<bool> visited;
     vector<vector<int>> vec_edges;
+    int cnt = 0;
 public:
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         vec_edges.resize(n);
+        visited.resize(n);
         
         // construct graph
         for (int i = 0; i < edges.size(); i++) {
@@ -14,7 +16,7 @@ public:
         
         // Transfer restricted vector to set
         for (int i = 0; i < restricted.size(); i++) {
-            visited.insert(restricted[i]);  // 直接加入 visited 即可，后面可以少一个判断
+            visited[restricted[i]] = true;  // 直接加入 visited 即可，后面可以少一个判断
         }
         //set_res = unordered_set<int>(restricted.begin(), restricted.end());
         
@@ -23,14 +25,15 @@ public:
         
         
         
-        return visited.size() - restricted.size();
+        return cnt;
     }
     
     void dfs(int num) {
-        visited.insert(num);
+        visited[num] = true;
+        cnt++;
         for (int i = 0; i < vec_edges[num].size(); i++) {
             int next = vec_edges[num][i];
-            if (visited.find(next) == visited.end()) {
+            if (!visited[next]) {
                 dfs(next);
             }
         }
