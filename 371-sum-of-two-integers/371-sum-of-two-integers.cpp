bb44a1db5c82631b4929fb377e0175ee1d713c4f
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-08-09 22:49:51 -0400

    Time: 0 ms (100.00%), Space: 6 MB (26.98%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        if(a==0)
+        if (a == 0)
             return b;
-        if(b==0)
+        if (b == 0)
             return a;
         
         int sum = 0;
         int mask = 1;
-        int c=0;
+        int c = 0;
         
-        while(mask!=0)
+        while(mask != 0)
         {
-            int m1 = (a &mask)!=0?1:0;
-            int m2 = (b &mask)!=0?1:0;
+            int m1 = (a & mask) != 0 ? 1:0;
+            int m2 = (b & mask) != 0 ? 1:0;
             
-            if(m1==1 && m2==1)
+            if(m1 == 1 && m2 == 1)
             {
                 if(c==1)
-                    sum|=mask;
+                    sum |= mask;
                 
                 c=1;
             }
             
-            else if((m1^m2)==1)
+            else if((m1^m2) == 1)
             {
-                if(c==1)
-                    c=1;
+                if(c == 1)
+                    c = 1;
                 else
-                    sum |=mask;
+                    sum |= mask;
             }
             else 
             {
-                if(c==1)
+                if(c == 1)
                 {
-                    sum|=mask;
+                    sum |= mask;
                 }
-                c=0;
+                c = 0;
             }
           
-            mask= (0xffffffff &mask)<<1;
+            mask= (0xffffffff & mask)<<1;
             
-            if(mask>2*max((a&0xffffffff)<<1,(b&0xffffffff)<<1))
+            if(mask > 2 * max((a&0xffffffff)<<1, (b&0xffffffff)<<1))
                 break;
         }
     
