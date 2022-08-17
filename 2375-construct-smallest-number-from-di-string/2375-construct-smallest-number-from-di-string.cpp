bf60df5f2b20ce832e72a5bafa8fd8d7b793f461
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-08-17 16:03:01 -0400

    Time: 4 ms (49.94%), Space: 5.8 MB (98.03%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,67 +1,17 @@
 class Solution {
+    // https://leetcode.com/problems/construct-smallest-number-from-di-string/discuss/2422380/JavaC%2B%2BPython-Easy-Reverse
 public:
-    string res="987654321";
-    string smallestNumber(string pattern) {
-        string s="";
-        vector<int> vis(10,0);
-        fun(vis,pattern,-1,s);
+    string smallestNumber(string s) {
+        string res, stack;
+        for (int i = 0; i <= s.length(); i++) {
+            stack.push_back('1' + i);
+            if (i == s.length() || s[i] == 'I') {
+                while (!stack.empty()) {
+                    res.push_back(stack.back());
+                    stack.pop_back();
+                }
+            }
+        }
         return res;
-    }
-    void fun(vector<int>& vis,string& p,int i,string& s)
-    {
-        if(i==p.length())
-        {
-            if(res>s)
-                res=s;
-            return;
-        }
-        
-        if(i==-1)
-        {
-            for(int j=1;j<=9;j++)
-            {
-                if(vis[j]==0)
-                {
-                    vis[j]=1;
-                    s+=j+'0';
-                    fun(vis,p,i+1,s);
-                    s.pop_back();
-                    vis[j]=0;
-                }
-            }
-        }
-        else
-        {
-            if(p[i]=='D')
-            {
-                int t=s[s.length()-1]-'0';
-                for(int j=t-1;j>=1;j--)
-                {
-                    if(vis[j]==0)
-                    {
-                        vis[j]=1;
-                        s+=j+'0';
-                        fun(vis,p,i+1,s);
-                        s.pop_back();
-                        vis[j]=0;
-                    }
-                }
-            }
-            else
-            {
-                int t=s[s.length()-1]-'0';
-                for(int j=t+1;j<=9;j++)
-                {
-                    if(vis[j]==0)
-                    {
-                        vis[j]=1;
-                        s+=j+'0';
-                        fun(vis,p,i+1,s);
-                        s.pop_back();
-                        vis[j]=0;
-                    }
-                }
-            }
-        }
     }
 };
