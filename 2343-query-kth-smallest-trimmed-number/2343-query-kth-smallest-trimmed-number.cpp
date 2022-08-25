7e894e67de7b5a1f400cda7bbed71c4d41fa71d5
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-08-24 21:25:57 -0400

    Time: 1425 ms (78.36%), Space: 160 MB (90.08%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,40 +1,37 @@
 class Solution {
+    // clarify: trim >= 1;  ki >= 1
+    // Time: O(N * M)
 private:    
-    struct val_index {
-        string_view val;
-        int index;
-    };
+    static int cmp(const pair<string_view, int>& p1, const pair<string_view, int>& p2) {
+        if (p1.first == p2.first)
+            return p1.second < p2.second;
+        
+        return p1.first < p2.first;
+    }
     
 public:
-    static int cmp(const val_index& VI1, const val_index& VI2) {
-        if (VI1.val != VI2.val) {
-            return VI1.val < VI2.val;
-        } else { // ==
-            return VI1.index < VI2.index;
-        }  
-    }
     
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
         vector<int> res;
-        int len = nums[0].size();
         
-        
-        // iterate each queries
         for (int i = 0; i < queries.size(); i++) {
-            int last = queries[i][1];
-            int th = queries[i][0];
-            vector<val_index> sort_vec;
+            int k = queries[i][0];
+            int trim = queries[i][1];
+            vector<pair<string_view, int>> nums_trim;  // store the trimed nums vector
+                                            // pair<val, idx_nums>
             
+            // construct nums_trim
             for (int j = 0; j < nums.size(); j++) {
-                val_index VI;
-                VI.index = j;
-                VI.val = string_view(nums[j].c_str() + len - last);
-                sort_vec.push_back(VI);
+                string_view sv = string_view(nums[j].c_str() + nums[j].size() - trim); // trim
+                nums_trim.push_back(pair<string_view, int>(sv, j));
             }
             
-            sort(sort_vec.begin(), sort_vec.end(), cmp);
-            res.push_back(sort_vec[th - 1].index);
+            
+            sort(nums_trim.begin(), nums_trim.end(), cmp);  // sort trimed strings
+            res.push_back(nums_trim[k - 1].second); // k >= 1
         }
+        
+        
         
         return res;
     }
