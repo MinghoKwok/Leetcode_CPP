7cd3b265cd2168a94753fd6032c2a6e4c22d4071
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-09-01 10:40:34 -0400

    Time: 33 ms (45.66%), Space: 11.1 MB (21.49%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mp;   // num -> indice
+        vector<int> res;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = mp.find(target - nums[i]);
+            if (it != mp.end()) {
+                res.push_back(i);
+                res.push_back(it->second);
+                break;
+            }
+            
+            mp[nums[i]] = i;
+        }
+        
+        
+        return res;
+    }
+};
