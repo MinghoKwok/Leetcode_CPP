cdd87349b190092f164ad20cc6b152eb0e5da4cb
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-09-02 17:05:02 -0400

    Time: 24 ms (19.92%), Space: 7.3 MB (79.04%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<char> s_vec;
-        vector<char> t_vec;
         
-        s_vec.assign(s.begin(), s.end());
-        t_vec.assign(t.begin(), t.end());
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
         
-        sort(s_vec.begin(), s_vec.end());
-        sort(t_vec.begin(), t_vec.end());
-        
-        return s_vec == t_vec;
+        return s == t;
     }
 };
